58d41683e8240f53eeb40336350a253cd6eddc18
Your Name <acolllen@myuax.com>
2025-11-14 13:07:34 +0100

    Actualizaciób del código y del README.md.

--- fibonacciRecursiva.c
+++ fibonacciRecursiva.c
@@ -1,7 +1,14 @@
-#include stdio.h
+#include <stdio.h>
 
 long long fibonacci(int numero) {
     if (numero == 0) return 0;
     if (numero == 1) return 1;
     return fibonacci(numero - 1) + fibonacci(numero - 2);
+}
+
+int main() {
+    int numero;
+
+    printf("Ingrese un número entero para el número de términos de la serie Fibonacci: ");
+    scanf("%d", &numero);
 }
