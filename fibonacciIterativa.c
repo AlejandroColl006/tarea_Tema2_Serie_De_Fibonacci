7a58d68b06c1d9aa0cbb2f43d9e6e8a48ae58d71
Your Name <acolllen@myuax.com>
2025-11-14 11:17:25 +0100

    Introducción de la declaración de variables long long ("a", "b" y "cambioDeVariable" y actualización del README.md.

--- fibonacciIterativa.c
+++ fibonacciIterativa.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  int numero;
-  printf ("Ingresa un número entero: ");
-  scanf ("%d", &numero);
+    int numero;
+    printf ("Ingresa un número entero: ");
+    scanf ("%d", &numero);
 
-  if (numero <= 0) {
-    printf ("Porfavor, ingresa un número entero que sea mayor que 0.");
-      return 0;
+    if (numero <= 0) {
+        printf ("Porfavor, ingresa un número entero que sea mayor que 0.");
+        return 0;
+    }
+
+    long long a = 0;
+    long long b = 1;
+    long long cambioDeVariable;
 }
