c462abc4581d2eac82f731cf4ed8b0b7aafada84
Your Name <acolllen@myuax.com>
2025-11-14 12:19:07 +0100

    Introducción de la función for (bucle) además del interior de la función, con salto de línea y actualización del README.md.

--- fibonacciIterativa.c
+++ fibonacciIterativa.c
@@ -6,11 +6,21 @@ int main() {
     scanf ("%d", &numero);
 
     if (numero <= 0) {
-        printf ("Porfavor, ingresa un número entero que sea mayor que 0.");
+        printf ("Porfavor, ingresa un número entero que sea mayor que 0. \n");
         return 0;
     }
 
     long long a = 0;
     long long b = 1;
     long long cambioDeVariable;
+
+    printf ("Serie de Fibonacci Iterativa (%d términos: \n", numero);
+
+    for (int i = 0; i < numero; i++) {
+        cambioDeVariable = b;
+        b = a + b;
+        a = cambioDeVariable;
+    }
+    printf ("\n");
+    return 0;
 }
